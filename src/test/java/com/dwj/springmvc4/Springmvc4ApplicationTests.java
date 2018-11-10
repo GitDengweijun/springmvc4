fc174b29e06953329a7aa22b6849106dbb71dfca
dengweijun <1625853957@qq.com>
2018-11-10 12:03:07 +0800

    initial springmvc4

---
+++ src/test/java/com/dwj/springmvc4/Springmvc4ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dwj.springmvc4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Springmvc4ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
